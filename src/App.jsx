7dc29543aebaf6f2026adc1f0e06c50dfd14983a
Hossaini <1989hossaini1@gmail.com>
2024-02-29 10:12:47 +0100

    taks 1 (Navbar) completed

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,21 @@
 import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+
+import Container from 'react-bootstrap/Container';
+import Navbar from 'react-bootstrap/Navbar';
 
 function App() {
-  const [count, setCount] = useState(0)
-
+  
   return (
     <>
-  
+      <Navbar expand="lg" className="">
+      <Container>
+        <Navbar.Brand href="#">🌐 Movie-Verve</Navbar.Brand>
+      </Container>
+    </Navbar>
+
+
+     
+
     </>
   )
 }
