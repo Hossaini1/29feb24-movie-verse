4c507053ffcdc5ee296c9c833d7b4d13bfecb247
Hossaini <1989hossaini1@gmail.com>
2024-03-01 19:11:44 +0100

    Komponente aufteilen

--- src/App.jsx
+++ src/App.jsx
@@ -1,168 +1,83 @@
 import { useEffect, useState } from "react";
-import { Col, Row, Image } from "react-bootstrap";
 import Container from "react-bootstrap/Container";
-import Navbar from "react-bootstrap/Navbar";
-import Img from "./assets/image/the-movie-verse.png";
-import { TbHeartMinus } from "react-icons/tb";
-
-// import avengersData from './moviesData';
-
-import Card from "react-bootstrap/Card";
-import { RiHeartAddLine } from "react-icons/ri";
+import { HeroSection } from "./components/HeroSection";
+import { NavBar } from "./components/NavBar";
+import { SearchArea } from "./components/SearchArea";
+import { MovieList } from "./components/MovieList";
+import { FavoriteList } from "./components/FavoriteList";
 
 function App() {
-  const [movies, setMovies] = useState([]);
-  const [searchValue, setSearchValue] = useState("");
-  const [favorites, setFavorites] = useState([]);
+	const [movies, setMovies] = useState([]);
+	const [searchValue, setSearchValue] = useState("");
+	const [favorites, setFavorites] = useState([]);
 
-  const getMovieRequest = async (searchValue) => {
-    const url = `https://www.omdbapi.com/?s=${searchValue}&apikey=${
-      import.meta.env.VITE_API_KEY
-    }`;
-    try {
-      const response = await fetch(url);
-      const responseJson = await response.json();
-      // console.log(responseJson);
+	useEffect(() => {
+		getMovieRequest(searchValue);
+	}, [searchValue]);
 
-      if (responseJson.Search) {
-        setMovies(responseJson.Search);
-      }
-    } catch (error) {
-      console.error(error);
-    }
-  };
+	useEffect(() => {
+		const favoriteMovies = JSON.parse(
+			localStorage.getItem("favorite-movies")
+		);
 
-  // getMovieRequest('alvin')
-  // console.log(avengersData);
+		if (favoriteMovies) {
+			setFavorites(favoriteMovies);
+		}
+	}, []);
 
-  useEffect(() => {
-    getMovieRequest(searchValue);
-  }, [searchValue]);
+	const getMovieRequest = async (searchValue) => {
+		const url = `https://www.omdbapi.com/?s=${searchValue}&apikey=${
+			import.meta.env.VITE_API_KEY
+		}`;
+		try {
+			const response = await fetch(url);
+			const responseJson = await response.json();
+			if (responseJson.Search) {
+				setMovies(responseJson.Search);
+			}
+		} catch (error) {
+			console.error(error);
+		}
+	};
 
-  useEffect(() => {
-    // LADEN aus localStorage unter der name "favorite-movies" und in favoriteMovies speichern
-    const favoriteMovies = JSON.parse(localStorage.getItem("favorite-movies"));
+	const saveToLocalStorage = (items) => {
+		localStorage.setItem("favorite-movies", JSON.stringify(items));
+	};
 
-    if (favoriteMovies) {
-      setFavorites(favoriteMovies);
-    }
-  }, []);
+	const removeFavoriteMovie = (movie) => {
+		const newFavoriteList = favorites.filter(
+			(favourite) => favourite.imdbID !== movie.imdbID
+		);
+		setFavorites(newFavoriteList);
+		saveToLocalStorage(newFavoriteList);
+	};
 
-  // SPICHERN in localStorage unter der name "favorite-movies"
-  const saveToLocalStorage = (items) => {
-    localStorage.setItem("favorite-movies", JSON.stringify(items));
-  };
+	const addToFavorites = (movie) => {
+		const foundMovie = favorites.find((m) => m.imdbID === movie.imdbID);
+		if (!foundMovie) {
+			const newFavorites = [...favorites, movie];
+			setFavorites(newFavorites);
+			saveToLocalStorage(newFavorites);
+		}
+	};
 
-  // FAVORITEN entfernen mit filter methode
-  const removeFavoriteMovie = (movie) => {
-    const newFavoriteList = favorites.filter(
-      (favourite) => favourite.imdbID !== movie.imdbID
-    );
-    // alter Liste überschreiben
-    setFavorites(newFavoriteList);
-    saveToLocalStorage(newFavoriteList);
-  };
-
-  const addToFavorites = (value) => {
-    // console.log(value);
-    const newFavorites = [...favorites, value];
-    setFavorites(newFavorites);
-    saveToLocalStorage(newFavorites);
-  };
-
-  return (
-    <>
-      {/* Navbar */}
-      <Navbar expand="lg" className="">
-        <Container>
-          <Navbar.Brand href="#">🌐 Movie-Verve</Navbar.Brand>
-        </Container>
-      </Navbar>
-      <Container>
-        {/* Hero section 1 */}
-        <Col className="d-flex pt-4 gap-3 align-items-center ">
-          <Row>
-            <h1 className="hero-text">Welcome to the Movie Verse</h1>
-            <p className="hero-description">
-              Find and save your favorite movies
-            </p>
-          </Row>
-          <Row>
-            <Image src={Img} alt="Hero image" />
-          </Row>
-        </Col>
-
-        {/* Hero section 2 */}
-        <div className="row d-flex align-items-center mt-4 ">
-          <div className="col">
-            <h2>MOVIE LIST:</h2>
-          </div>
-          <div className="col">
-            <input
-              value={searchValue}
-              onChange={(e) => setSearchValue(e.target.value)}
-              type="text"
-              placeholder="Search a movie..."
-              className="form-control"
-            />
-          </div>
-        </div>
-
-        <div className="row nowrap ">
-          {movies.map((movie) => {
-            return (
-              <Card
-                className="pt-3 m-3"
-                key={movie.imdbID}
-                style={{ width: "18rem" }}
-              >
-                <Card.Img variant="top" src={movie.Poster} />
-                <Card.Body className="d-flex align-items-center justify-content-between">
-                  <Card.Title>
-                    {movie.Title} ({movie.Year})
-                  </Card.Title>
-                  <RiHeartAddLine
-                    onClick={() => addToFavorites(movie)}
-                    className="like-icon"
-                  />
-                </Card.Body>
-              </Card>
-            );
-          })}
-        </div>
-
-        {/* Favorite list section */}
-        <div className="row d-flex align-items-center mt-4 ">
-          <div className="col">
-            <h2>Favorites Movies:</h2>
-          </div>
-        </div>
-        <div className="row nowrap ">
-          {favorites.map((movie) => {
-            return (
-              <Card
-                className="pt-3 m-3"
-                key={movie.imdbID}
-                style={{ width: "18rem" }}
-              >
-                <Card.Img variant="top" src={movie.Poster} />
-                <Card.Body className="d-flex align-items-center justify-content-between">
-                  <Card.Title>
-                    {" "}
-                    {movie.Title} ({movie.Year})
-                  </Card.Title>
-                  <TbHeartMinus
-                    className="like-icon"
-                    onClick={() => removeFavoriteMovie(movie)}
-                  />
-                </Card.Body>
-              </Card>
-            );
-          })}
-        </div>
-      </Container>
-    </>
-  );
+	return (
+		<>
+			<NavBar />
+			<Container>
+				<HeroSection />
+				<SearchArea
+					searchValue={searchValue}
+					setSearchValue={setSearchValue}
+				/>
+				<MovieList movies={movies} addToFavorites={addToFavorites} favorites={favorites} removeFavoriteMovie={removeFavoriteMovie} />
+				<FavoriteList
+					favorites={favorites}
+					removeFavoriteMovie={removeFavoriteMovie}
+				/>
+			</Container>
+		</>
+	);
 }
 
 export default App;
